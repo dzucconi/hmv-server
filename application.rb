a3c0bb5243b4624abe1988f25ddd7366620988a6
dzucconi <mail@damonzucconi.com>
2016-01-23 16:55:25 -0500

    Compile more asset types

--- application.rb
+++ application.rb
@@ -1,5 +1,5 @@
 class Application < Sinatra::Base
-  set :assets_precompile, %w(application.css application.js)
+  set :assets_precompile, %w(application.css application.js *.ico *png *.svg *.woff *.woff2)
   set :assets_prefix, %w(assets)
   set :assets_css_compressor, :sass
   set :protection, except: [:frame_options]
