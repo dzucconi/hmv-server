f316f8d2b6881b2f72bb26bff9b619893c0cf218
dzucconi <mail@damonzucconi.com>
2016-01-23 16:55:34 -0500

    Use "template"

--- application.rb
+++ application.rb
@@ -7,38 +7,20 @@ class Application < Sinatra::Base
   register Sinatra::AssetPipeline
 
   get '/' do
-    HTML.new.instance_eval do
-      page do
-        form(class: 'js-form') do
-          textarea(
-            class: 'js-input', placeholder: 'Begin typing, then <cmd> + <enter> to play', autofocus: 'true') { '' } +
-
-          div(class: 'controls js-controls') do
-            button(class: 'js-submit') { 'Play' }
-          end
-        end +
-
-        div(class: 'progress') do
-          div(class: 'progress-bar js-progress')
-        end
-      end
-    end
+    Template::Index.page
   end
 
   get '/render.wav' do
     content_type 'audio/wav'
-
     raise Exception if params[:text].nil?
-
     words = Corrasable.new(params[:text]).to_phonemes
-    output = Output.new(words, params[:type])
-
+    output = Output.new(words, params[:duration], params[:type])
+    output.generate!
     File.open(output.filename).read
   end
 
   error Exception do
     status 400
-
     'Bad Request'
   end
 end
