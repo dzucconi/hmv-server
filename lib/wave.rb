b5f6ea5bc8f7b98f92dcbd775fe11cbb02e7b37d
dzucconi <mail@damonzucconi.com>
2016-03-03 08:02:58 -0500

    Some wave utilities for cli output

--- lib/wave.rb
+++ lib/wave.rb
@@ -1,9 +1,25 @@
 class Wave
   include WaveFile
 
-  def self.concat(buffers, rate = 44100, file_name = SecureRandom.uuid)
+  def self.concat(buffers, rate = Synthetic::SAMPLE_RATE, file_name = SecureRandom.uuid)
     Writer.new("tmp/#{file_name}.wav", Format.new(:mono, :pcm_16, rate)) { |writer|
       buffers.each { |buffer| writer.write buffer }
+    }.file_name
+  end
+
+  def self.concat_files(file_names, rate = Synthetic::SAMPLE_RATE, file_name = SecureRandom.uuid)
+    Writer.new("tmp/#{file_name}.wav", Format.new(:mono, :pcm_16, rate)) { |writer|
+      file_names.each do |fn|
+        Reader.new(fn).each_buffer(rate) do |buffer|
+          writer.write(buffer)
+        end
+      end
+    }.file_name
+  end
+
+  def self.buffer_to_file(buffer, rate = Synthetic::SAMPLE_RATE, file_name = SecureRandom.uuid)
+    Writer.new("tmp/#{file_name}.wav", Format.new(:mono, :pcm_16, rate)) { |writer|
+      writer.write buffer
     }.file_name
   end
 end
