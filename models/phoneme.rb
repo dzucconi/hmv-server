2532aaf19cccd6f0dc5ff070205668167ede93f6
dzucconi <mail@damonzucconi.com>
2016-10-31 13:53:20 +0100

    Refactor to use a notational lib and increase the range of tonality

--- models/phoneme.rb
+++ models/phoneme.rb
@@ -1,6 +1,9 @@
 class Phoneme
   attr_reader :phoneme
 
+  OFFSET = 3 # Push scale up
+  DURATION_BASE = 3_000.0
+
   def initialize(phoneme)
     @phoneme = case phoneme
       when 'N/A'
@@ -8,20 +11,32 @@ class Phoneme
       when ' '
         :PAUSE
       else
-        phoneme.upcase.gsub(/[^a-z ]/i, '').to_sym
+        phoneme.to_s.upcase.gsub(/[^a-z ]/i, '').to_sym
       end
   end
 
   def duration
-    Phonemes.get(phoneme)[:duration] / 3_000.0
+    Phonemes.get(phoneme)[:duration] / DURATION_BASE
   end
 
   def index
     Phonemes.collection.keys.index(phoneme)
   end
 
+  def octave
+    ((index + 1) / (Phonemes.length / Phonemes.octaves)).ceil + OFFSET
+  end
+
+  def latin
+    (Phonemes.notes[index % Phonemes.notes.length]).to_s + octave.to_s
+  end
+
   def note
-    index - (Phonemes.collection.keys.size / 2)
+    @note ||= Note.from_latin(latin)
+  end
+
+  def frequency
+    note.frequency
   end
 
   def to_json(options = {})
