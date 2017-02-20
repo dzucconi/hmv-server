842ec49b1a0ef3a20bf98cbeca69f1b8a6831b64
dzucconi <mail@damonzucconi.com>
2017-02-20 20:41:00 +0100

    Simplifies Storage module

--- lib/storage.rb
+++ lib/storage.rb
@@ -1,28 +1,18 @@
 # frozen_string_literal: true
 module Storage
+  BUCKET = ENV['S3_BUCKET']
+
   class << self
     def connection
       @connection ||= Fog::Storage.new(
-        aws_access_key_id: access_key_id,
-        aws_secret_access_key: secret_access_key,
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
         provider: 'AWS'
       )
     end
 
     def bucket
-      @bucket ||= connection.directories.get(s3_bucket)
-    end
-
-    def s3_bucket
-      ENV['S3_BUCKET']
-    end
-
-    def access_key_id
-      ENV['AWS_ACCESS_KEY_ID']
-    end
-
-    def secret_access_key
-      ENV['AWS_SECRET_ACCESS_KEY']
+      @bucket ||= connection.directories.get(BUCKET)
     end
 
     def get(key)
@@ -30,8 +20,14 @@ module Storage
     end
 
     def set(io, key)
-      object = bucket.files.create(key: key, public: true, body: io.read)
+      object = bucket.files.create(
+        key: key,
+        public: true,
+        body: io.read
+      )
+
       io.rewind
+
       [io, object]
     end
 
