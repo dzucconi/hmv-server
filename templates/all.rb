cc45d20f008f93166e144ab5ec9f9bfe5be079d4
dzucconi <mail@damonzucconi.com>
2017-03-08 15:50:04 +0100

    Removes delete buttons when logged out

--- templates/all.rb
+++ templates/all.rb
@@ -1,12 +1,12 @@
 module Template
   class All
-    def self.page(_user, pieces)
+    def self.page(user, pieces)
       HTML.new.instance_eval do
         div(class: 'pieces') do
           pieces.map do |piece|
             div(class: 'piece') do
               a(href: "/#{piece.key}") { piece.input } + ' ' +
-              a(class: 'delete js-delete', 'data-key' => piece.key) { 'Delete' }
+              (user.authentic? ? a(class: 'delete js-delete', 'data-key' => piece.key) { 'Delete' } : '')
             end
           end.join('')
         end
