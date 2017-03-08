d751a7056ce6f6bc18967e971b75afe41df085b9
dzucconi <mail@damonzucconi.com>
2017-03-08 14:41:47 +0100

    Adds placeholders

--- templates/login.rb
+++ templates/login.rb
@@ -3,8 +3,8 @@ module Template
     def self.page(_ = nil)
       HTML.new.instance_eval do
         form(action: '/login', method: 'POST') do
-          input(type: 'text', name: 'username') +
-          input(type: 'password', name: 'password') +
+          input(type: 'text', name: 'username', placeholder: 'username') +
+          input(type: 'password', name: 'password', placeholder: 'password') +
           div(class: 'controls') do
             button { 'Login' }
           end
